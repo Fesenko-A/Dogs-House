c1dea0422c55650c2f09ea14767e82f21a92c570
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-07-28 14:35:49 +0300

    add DataAccess.Configuration project for DI container

---
+++ DataAccess.Configuration/DataAccessServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataAccess.Contracts.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using DataAccess.Repositories.EntityFramework;
+using DataAccess.Repositories.EntityFramework.Data;
+
+namespace DataAccess.Configuration {
+    public static class DataAccessServices {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services, string connectionString) {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddScoped<IDogRepository, DogRepository>();
+            return services;
+        }
+    }
+}
