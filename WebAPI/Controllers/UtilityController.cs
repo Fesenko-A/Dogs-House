799a1d825c95cf600b8fa032ff2349c6242cdc72
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-07-30 18:39:46 +0300

    add UtilityController with a Ping endpoint

---
+++ WebAPI/Controllers/UtilityController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers {
+    [Route("")]
+    [ApiController]
+    public class UtilityController : ControllerBase {
+        [HttpGet("Ping")]
+        public ActionResult Get() => Ok("Dogshouseservice.Version1.0.1");
+    }
+}
